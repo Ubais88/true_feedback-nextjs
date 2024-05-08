4c77c1fdceacf5aa3024be303fd5e916b21e3b03
Ubais88 <moubais2143@gmail.com>
2024-05-08 17:37:17 +0530

    verifiation email added

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,3 +1,30 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { NextResponse } from "next/server";
 
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "True Feedback Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
 
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+    };
+  } catch (error) {
+    console.log("error during sending verification email: ", error);
+    return {
+      success: false,
+      message: "Verification email failed to be sent",
+    };
+  }
+}
