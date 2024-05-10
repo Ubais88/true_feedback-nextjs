729c4d19ba035ef051db1c0919b188f101b4a639
Ubais88 <moubais2143@gmail.com>
2024-05-10 16:33:43 +0530

    verify code added

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -68,7 +68,7 @@ export default function SignUpForm() {
   const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
     setIsSubmitting(true);
     try {
-      const response = await axios.post<ApiResponse>("/api/sign-up", data);
+      const response = await axios.post<ApiResponse>("/api/signup", data);
 
       toast({
         title: "Success",
@@ -98,7 +98,7 @@ export default function SignUpForm() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+    <div className="flex justify-center items-center min-h-screen bg-gray-300">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,50 +1,62 @@
 import UserModel from "@/model/User";
-import dbConnect from "@/lib/dbConnect"; 
+import dbConnect from "@/lib/dbConnect";
 
-export async function POST(request:Request){
-    await dbConnect();
-    try {
-        const { username , code } = await request.json();
-        console.log("normal username: " , username)
-        const decodedUsername = decodeURIComponent(username)
-        console.log("decodedUsername: ", decodedUsername)
-        const user = await UserModel.findOne({ username : decodedUsername});
-        if(!user){
-            return Response.json({
-                success:false,
-                message:"User not found"
-            }, {status:400})
-        }
-        const isCodeValid = user.verifyCode === code ;
-        const isCodeNotExpiry = new Date(user.verifyCodeExpiry) > new Date() ;
-        if(isCodeValid && isCodeNotExpiry){
-            user.isVerified = true;
-            await user.save();
-
-            return Response.json({
-                success:true,
-                message:"User is now verified"
-            }, {status:200})
-        }
-        else if(!isCodeNotExpiry){
-            return Response.json({
-                success:false,
-                message:"code is expired"
-            }, {status:400})
-        }
-        else{
-            return Response.json({
-                success:false,
-                message:"code is invalid"
-            }, {status:400})
-        }
-
-        
-    } catch (error) {
-        console.log("error during code verification: ", error)
-        return Response.json({
-            success:false,
-            message:"Error during code verification"
-        }, {status:400})
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    console.log("normal username: ", username);
+    console.log("verify code : ", code);
+    const decodedUsername = decodeURIComponent(username);
+    console.log("decodedUsername: ", decodedUsername);
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 400 }
+      );
     }
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpiry = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpiry) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        {
+          success: true,
+          message: "User is now verified",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpiry) {
+      return Response.json(
+        {
+          success: false,
+          message: "code is expired",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "code is invalid",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log("error during code verification: ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error during code verification",
+      },
+      { status: 400 }
+    );
+  }
 }
